c81feb2110c6d7064c05328f9769efbd49013102
hensell <hensell04@hotmail>
2024-12-20 03:23:56 -0600

    Added config deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix:  '/hen-counter/' ,
+  basePath: '/hen-counter',
+  
 };
 
 export default nextConfig;
